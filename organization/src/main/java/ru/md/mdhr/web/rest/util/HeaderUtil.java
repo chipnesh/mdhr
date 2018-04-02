1c78626550601eed5b100be07b60e71809535721
mi_vysokih <dvysokih@mobiledimension.ru>
2018-04-02 22:44:35 +0300

    'fixes'

--- organization/src/main/java/ru/md/mdhr/web/rest/util/HeaderUtil.java
+++ organization/src/main/java/ru/md/mdhr/web/rest/util/HeaderUtil.java
@@ -11,7 +11,7 @@ public final class HeaderUtil {
 
     private static final Logger log = LoggerFactory.getLogger(HeaderUtil.class);
 
-    private static final String APPLICATION_NAME = "organizationApp";
+    private static final String APPLICATION_NAME = "webApp";
 
     private HeaderUtil() {
     }
